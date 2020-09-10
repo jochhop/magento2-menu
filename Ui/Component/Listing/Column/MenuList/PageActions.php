a126b3a725ec40965e99fbff6b2f939d1d5e3bd3
Jose Ortega <jose.ortega@snow.dog>
2020-09-10 10:52:07 +0200

    Setting up part of the skeleton for the new import/export features

--- Ui/Component/Listing/Column/MenuList/PageActions.php
+++ Ui/Component/Listing/Column/MenuList/PageActions.php
@@ -11,19 +11,46 @@ class PageActions extends Column
             foreach ($dataSource["data"]["items"] as & $item) {
                 $name = $this->getData("name");
                 $id = "X";
+
                 if (isset($item["menu_id"])) {
                     $id = $item["menu_id"];
                 }
-                $item[$name]["view"] = [
-                    "href"  => $this->getContext()->getUrl(
-                        "snowmenu/menu/edit",
-                        ["id" => $id]
-                    ),
-                    "label" => __("Edit"),
-                ];
+
+                $item[$name]["view"] = $this->getEditAction($id);
+                $item[$name]["export"] = $this->getExportAction($id);
             }
         }
 
         return $dataSource;
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    private function getEditAction(int $id): array
+    {
+        return [
+            "href" => $this->getContext()->getUrl(
+                "snowmenu/menu/edit",
+                ["id" => $id]
+            ),
+            "label" => __("Edit"),
+        ];
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    private function getExportAction(int $id): array
+    {
+        return [
+            "href" => $this->getContext()->getUrl(
+                "snowmenu/menu/export",
+                ["id" => $id]
+            ),
+            "label" => __("Export"),
+        ];
     }
 }
